246a118186ce55cde3b28ab36dee5e4122f29d0a
Mohit Jatav <jatavmohit747@gmail.com>
2023-04-14 00:26:08 +0530

    added message in Sum.java

---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class multiply {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 5;
+
+        System.out.println("The Product of A & B is: " + a*b);
+    }
+}
