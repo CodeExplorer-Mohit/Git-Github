fdf5e735eaea375937852bfbd7adf726a23b1ac5
Mohit Jatav <jatavmohit747@gmail.com>
2023-04-13 23:59:06 +0530

    added message in Sum.java

--- Sum.java
+++ Sum.java
@@ -3,7 +3,7 @@ public class Sum{
         int a = 10;
         int b = 20;
 
-        System.out.println("Sum is: " + a+b);   
+        System.out.println("Sum of A and B is: " + a+b);   
     }
 
 }
